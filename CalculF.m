00a2a3ead3ed76a6a10c8f8b2d610c7d2a19183c
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-05-10 02:02:00 +0200

    ajout du film1, modif sparse dans Lap1D

--- CalculF.m
+++ CalculF.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function F=CalculF(f,t,x)
 % Calcul de la fonction f(t,x) 
 % INPUT :
@@ -12,19 +11,4 @@ nx=length(x);
 F=zeros(nx,nt);
 for n=1:nt
     F(:,n)=f(t(n),x)';
-=======
-function F=CalculF(f,t,x)
-% Calcul de la fonction f(t,x) 
-% INPUT :
-%   f :  fonction du type @(t,x)
-%   t :  discretisation en temps, vecteur de dimension nt
-%   x : discretisation en espace,  vecteur de dimension nx
-% OUTPUT :
-%   F : matrice definie par F(i,n)=f(t(n),x(i)), de dimension (nx,nt)
-nt=length(t);
-nx=length(x);
-F=zeros(nx,nt);
-for n=1:nt
-    F(:,n)=f(t(n),x)';
->>>>>>> de207de9f8c99e922567b870b4ca49fe8cd3d992
 end
--- Lap1D.m
+++ Lap1D.m
@@ -1,3 +1,5 @@
 function K=Lap1D(d)
-  K=-2*eye(d)+diag(ones(1,d-1),1)+diag(ones(1,d-1),-1);
+  I=[2:d 1:d 1:d-1];
+  J=[1:d-1 1:d 2:d];
+  K=sparse(I,J,[ones(1,d-1) -2*ones(1,d) ones(1,d-1)],d,d);
 endfunction
