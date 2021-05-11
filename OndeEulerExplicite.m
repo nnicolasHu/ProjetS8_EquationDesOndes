93c5959aefbac9cdfd2b7423428dd4c833f5dec9
ZalhataBahassane <ingenieur@DESKTOP-MTE998H.univ-paris13.fr>
2021-05-11 17:28:35 +0200

    Zalhat

--- OndeEulerExplicite.m
+++ OndeEulerExplicite.m
@@ -11,14 +11,16 @@ function [t,x,u,dx,dt]=OndeEulerExplicite(EDP,Nt,Nx)
   alpha=((c*dt)/dx)^2;
   u=zeros(Nx+1,Nt+1);
   u1=zeros(Nx+1,1);
-  u(1,:)=EDP.ua(t);
-  u(Nx+1,:)=EDP.ub(t);
+
+  u(1,2:end)=EDP.ua(t(2:end));
+  u(Nx+1,2:end)=EDP.ub(t(2:end));
   u(:,1)=EDP.u0(x);
   u1(:,1)=EDP.u1(x);
-  u(:,2)=(eye(Nx+1)+(dt*dt/2)*c*c*Lap1D(Nx+1))*u(:,1)+dt*u1(:,1);
+  u(2:Nx,2)=(eye(Nx-1)+(dt*dt/2)*c*c*Lap1D(Nx-1))*u(2:Nx,1)+dt*u1(2:Nx,1);
+  
   for j=2:Nt
     for i=2:Nx
-      u(i,j+1)=2*(1-alpha^2)*u(i,j)+(alpha^2)*(u(i-1,j)+u(i+1,j))-u(i,j-1);
+      u(i,j+1)=2*(1-alpha)*u(i,j)+alpha*(u(i-1,j)+u(i+1,j))-u(i,j-1);
     endfor
   endfor
 endfunction
--- mainOndeEulerExplicite.m
+++ mainOndeEulerExplicite.m
@@ -10,20 +10,20 @@ l=1;
 EDP.a=-l;EDP.b=l;
 EDP.t0=0;EDP.T=2;
 EDP.c=1;
-EDP.uex=@(t,x) cos(((2*m+1)*pi*x)/(2*l))*cos(EDP.c*(((2*m+1)*pi)/(2*l))*t);
+EDP.uex=@(t,x) 0.5*(cos((2*m+1)*pi*(x+EDP.c*t)/(2*l))+cos((2*m+1)*pi*(x-EDP.c*t)/(2*l)));
 EDP.u0=@(x) cos(((2*m+1)*pi*x)/(2*l));
 EDP.ua=@(t) 0*t;
 EDP.ub=@(t) 0*t;
 EDP.u1=@(x) 0*x;
 
-dx=zeros(3,1);
-Err=zeros(3,1);
+dx=zeros(2,1);
+Err=zeros(2,1);
 % 2. Parametres de discretisation
 
 
-%Nt=2000; % => divergence
+Nx=500; % => divergence
 Nt=2100;% => convergence 
-Nx=[500 1000 2000];
+%Nx=[100 200 400];
 %Nx=150;
 %Nt=4500; % => divergence
 %Nt=5000; % => convergence 
@@ -31,8 +31,8 @@ Nx=[500 1000 2000];
 %  Nx=200;
 % Nt = 8000; % => divergence
 % Nt = 8400; % => convergence
-k=1;
-for i=Nx
+%k=1;
+%for i=Nx
   
   % 3. Verification de la condition de C.F.L.
   ht=EDP.T/Nt; hx=(EDP.b-EDP.a)/i;
@@ -41,31 +41,37 @@ for i=Nx
   disp(s);
 
   % 4. Resolution par le schema d'EULER explicite
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %     Fonction non fournie : A IMPLEMENTER
+  
   disp('Calcul en cours...')
-  [t,x,u,dx(k),dt]=OndeEulerExplicite(EDP,Nt,i);
+  [t,x,u,dx(k),dt]=OndeEulerExplicite(EDP,Nt,Nx);
   disp('Fin du calcul.')
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  Uex=CalculF(EDP.uex,t,x);
-  Err(k)=max(max(abs(u-Uex)));
-  k=k+1;
-end
+%  Uex=CalculF(EDP.uex,t,x);
+%  Err(k)=max(max(abs(u-Uex)));
+%  k=k+1;
+%end
 
 
 
-dx
-Err
-loglog(dx,Err);
+%dx
+%Err
+%loglog(dx,Err);
+%hold on
+%loglog(dx,dx.^2,'ks-');
 % 5. Representations graphiques
 Uex=CalculF(EDP.uex,t,x); % Solution exacte
 MIN=min(min(Uex));
 MAX=max(max(Uex));
-%if PLOT
-%    figure(1)
-%    PlotSol2(t,x,u,'freq',FREQ,'title','Sol. Appr.','axis',[x(1) x(end) MIN MAX],'pause',0.1)
-    % PlotSol(t,x,u,FREQ,'Sol. Appr.',[x(1) x(end) MIN MAX],0.1) % Old version
+if PLOT
+    figure(1)
+    PlotSol2(t,x,u,'freq',FREQ,'title','Sol. Appr.','axis',[x(1) x(end) MIN MAX],'pause',0.1)
+   
+    PlotSol2(t,x,Uex,'freq',FREQ,'title','Sol. Ex.','axis',[x(1) x(end) MIN MAX],'pause',0.1)
+    %PlotSol(t,x,u,FREQ,'Sol. Appr.',[x(1) x(end) MIN MAX],0.1) % Old version
 %end
+
+
+
 
 Err=abs(u-Uex)/MAX; % relative error
 MAX=max(max(Err));
@@ -90,4 +96,3 @@ Ninf=NormInf(Err);
 %end
 
 fprintf('Erreur relative  (max en temps et espace) : %e\n',max(Ninf)/max(max(abs(Uex))));
-
