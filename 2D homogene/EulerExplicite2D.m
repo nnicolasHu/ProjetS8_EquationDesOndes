af3f6433746eb8e220bbcf67d75d3aa2fb31c7d8
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-05-28 15:35:36 +0200

    début de la 2D

---
+++ 2D homogene/EulerExplicite2D.m
@@ -0,0 +1,15 @@
+function [t,x,y,u]=EulerExplicite2D(EDP,Nt,Nx,Ny)
+  dt=(EDP.T-EDP.t0)/Nt;
+  dx=(EDP.b-EDP.a)/Nx;
+  dy=(EDP.d-EDP.c)/Ny;
+  
+  t=EDP.t0 + dt*[0:Nt];
+  x=EDP.a + dx*[0:Nx];
+  y=EDP.c + dy*[0:Ny];
+  
+  %initialisation
+  u=zeros(Ny+1,Nx+1,Nt+1); %1er coord = y(ligne), 2è coord = x(colonne), 3è coord = temps(hauteur)
+  u(:,:,1)=calculEspace(EDP.u0,x,y);
+  
+  
+endfunction
