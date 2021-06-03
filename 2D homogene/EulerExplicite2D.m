3ec93bdcda91490e65b0f7a202f350b04d605b6b
Nicolas Hu <courriel.nicolashu@gmail.com>
2021-06-04 01:24:26 +0200

    add de Lap2D

--- 2D homogene/EulerExplicite2D.m
+++ 2D homogene/EulerExplicite2D.m
@@ -1,7 +1,7 @@
 function [t,x,y,u]=EulerExplicite2D(EDP,Nt,Nx,Ny)
   dt=(EDP.T-EDP.t0)/Nt;
   dx=(EDP.b-EDP.a)/Nx;
-  dy=(EDP.d-EDP.c)/Ny;
+  dy=(EDP.b-EDP.a)/Ny;
   
   t=EDP.t0 + dt*[0:Nt];
   x=EDP.a + dx*[0:Nx];
@@ -9,7 +9,9 @@ function [t,x,y,u]=EulerExplicite2D(EDP,Nt,Nx,Ny)
   
   %initialisation
   u=zeros((Ny+1)*(Nx+1),Nt+1); %le carré en espace est "compresse" en un vecteur colone
-  %u(:,1)=calculEspace(EDP.u0,x,y);
+  u(:,1) = EvalFun2DVec(EDP.u0,x,y,Nx+1,Ny+1);
+  Lap2D = Lap2DAssembling(Nx+1,Ny+1,dx,dy);
+  u(:,2) = u(:,1) + dt*EvalFun2DVec(EDP.u1,x,y,Nx+1,Ny+1) + (dt*EDP.c)^2 /2 * Lap2D*u(:,1);
   
   
 endfunction
--- 2D homogene/Lap2DAssembling.m
+++ 2D homogene/Lap2DAssembling.m
@@ -4,5 +4,4 @@ function Axy=Lap2DAssembling(nx,ny,hx,hy)
   Ax=(1/(hx*hx))*spMatTriDiagVec([ones(1,nx-2) 0],[0 -2*ones(1,nx-2) 0],[0 ones(1,nx-2)]);
   Ay=(1/(hy*hy))*spMatTriDiagVec([ones(1,ny-2) 0],[0 -2*ones(1,ny-2) 0],[0 ones(1,ny-2)]);
   Axy=spMatKron(Jny,Ax)+spMatKron(Ay,Jnx);
-
 endfunction
